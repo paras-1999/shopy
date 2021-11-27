2354b627a8610d237af21a21333466f100cfa510
paras-1999 <paras.saxena@neosoftmail.com>
2021-11-28 01:16:00 +0530

    improved hover

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -35,9 +35,8 @@ export default function Home() {
         navigate('/')
     }
     return (
-        <div>
+        <>
             <h1>ShoPy <img src="http://www.stampready.net/dashboard/editor/user_uploads/zip_uploads/2018/03/16/4p1W6RcD97vSfBswLFMaonix/abandoned_cart/images/icon_shopping_cart.gif" style={{ height: "80px", width: "80px", borderRadius: "50%" }} /></h1>
-
             <Container>
                 <Row className="mx-0 my-4">
                     <Button as={Col} variant="info" className='me-2 catbtn' onClick={() => { setCat('all') }}>All</Button>
@@ -50,33 +49,26 @@ export default function Home() {
                 <Row>
                     <Col md={11}>
                         <Form.Floating className="mb-3">
-                            <Form.Control
-                                type="text"
-                                placeholder="xyz"
-                                ref={search}
-                            />
+                            <Form.Control type="text" placeholder="xyz" ref={search} />
                             <label>search</label>
-                        </Form.Floating></Col>
-                    <Button as={Col} variant="info" onClick={() => display()} style={{ height: "58px" }}><svg style={{ margin: "12px auto" }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-search" viewBox="0 0 16 16">
-                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                    </svg></Button>
+                        </Form.Floating>
+                    </Col>
+                    <Button as={Col} variant="info" onClick={() => display()} style={{ height: "58px" }}>
+                        <svg style={{ margin: "12px auto" }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                        </svg>
+                    </Button>
                 </Row>
             </Container>
             <Container>
-
                 <Row className="g-3">
                     {products.map((val, i) => {
                         if (cat == val.category) {
                             return (<Col md={4} xs={6} key={val.pname}>
                                 <Card className="bg1" style={{ height: "320px" }}>
-                                    <Card.Img variant="top" className="d-block w-80 mx-auto"
-                                        src={val.img1}
-                                        height="250px"
-                                        onMouseOver={e => (e.currentTarget.src = val.img2)}
-                                        onMouseOut={e => (e.currentTarget.src = val.img1)}
-                                    />
-
-
+                                    <a className="outer">
+                                        <span className="inner" style={{ backgroundImage: `url(${val.img1}),url(${val.img2} )` }}></span>
+                                    </a>
                                     <Card.Body>
                                         <Card.Title style={{ fontFamily: " 'Bebas Neue', cursive", fontWeight: "bolder" }}>{val.pname}<Button className="wbtn">{val.price}</Button></Card.Title>
                                     </Card.Body>
@@ -87,13 +79,8 @@ export default function Home() {
                         if (cat == 'all') {
                             return (<Col md={4} xs={6} key={val.pname}>
                                 <Card className={i % 2 ? "bg1" : "bg2"} style={{ height: "320px" }}>
-                                    <Card.Img variant="top" className="d-block w-80 mx-auto"
-                                        src={val.img1}
-                                        height="250px"
-                                        onMouseOver={e => (e.currentTarget.src = val.img2)}
-                                        onMouseOut={e => (e.currentTarget.src = val.img1)}
-                                    />
-
+                                    <a className="outer"> <span className="inner" style={{ backgroundImage: `url(${val.img1}),url(${val.img2} )` }}> </span>
+                                    </a>
                                     <Card.Body>
                                         <Card.Title style={{ fontFamily: " 'Bebas Neue', cursive", fontWeight: "bolder", color: "black" }}>{val.pname}<Button className="wbtn">{val.price}</Button></Card.Title>
                                     </Card.Body>
@@ -129,24 +116,23 @@ export default function Home() {
                     <Button variant="info" className="text-white" size="lg">buy at {selected.price}</Button>
                 </Modal.Body>
             </Modal>}
-
-        </div >
+        </>
     )
 }
-
-{/* <Carousel>
-                                        <Carousel.Item interval={1000}>
-                                            <img
-                                                className="d-block w-80 mx-auto"
-                                                src={`${val.img1}`}
-                                                height="250px"
-                                            />
-                                        </Carousel.Item>
-                                        <Carousel.Item interval={1000}>
-                                            <img
-                                                className="d-block w-100"
-                                                src={`${val.img2}`}
-                                                height="250px"
-                                            />
-                                        </Carousel.Item>
-                                    </Carousel> */}
+// first try
+// <Carousel>
+// <Carousel.Item interval={1000}>
+// <img className="d-block w-80 mx-auto" src={`${val.img1}`} height="250px"/>
+// </Carousel.Item>
+// <Carousel.Item interval={1000}>
+// <img className="d-block w-80 mx-auto" src={`${val.img1}`} height="250px"/>
+// </Carousel.Item>
+// </Carousel> 
+// second try
+// <Card.Img variant="top" 
+// className="d-block w-80 mx-auto"                                     
+// src={val.img1}                               
+// height="250px"                               
+// onMouseOver={e => (e.currentTarget.src = val.img2)}                          
+// onMouseOut={e => (e.currentTarget.src = val.img1)}
+// />
